d1653b6ce4709a8ec58469f57e4895dd9e62259a
SahasT23 <sahas.st01@gmail.com>
2024-07-14 17:47:16 +0100

    tried cache method and it is much faster, will add code to see memory usage and time

--- CacheTest.py
+++ CacheTest.py
@@ -1,7 +1,14 @@
+from functools import cache
+
+@cache
 def fibonacci(n):
   if n == 1 or n == 2:
     return 1
   else:
     return fibonacci(n-1) + fibonacci(n-2)
   
-print(fibonacci(5))
+
+
+# recursive process right now
+# made faster by becoming an iterative process
+# but we can use caching instead, more memory is used, but it is very fast, can use LRU instead
